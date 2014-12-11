9e6659da2066354717c63d0d1c25c63905164997
Chris Small <chsmall22@gmail.com>
2014-12-10 21:44:09 -0800

    Added test_client unit tests

--- odlclient/tests/unit/test_client.py
+++ odlclient/tests/unit/test_client.py
@@ -14,81 +14,42 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import json
+from odlclient.v2.client import BridgeDomainManager
+from odlclient.v2.client import Client
+from odlclient.v2.client import ConnectionManager
+
+from odlclient.v2.client import HTTPClient
+from odlclient.v2.client import NeutronManagers
+from odlclient.v2.client import NodeManager
+from odlclient.v2.client import OvsdbManager
+from odlclient.v2.client import StaticRouteManager
+from odlclient.v2.client import SubnetManager
+
 import unittest
-try:
-    from unittest.mock import MagicMock
-except ImportError:
-    from mock import MagicMock
-
-import requests
 
 
-from odlclient.v2 import client as odlclient
-
-
-class ClientTests(unittest.TestCase):
+class ClientTestCase(unittest.TestCase):
     def setUp(self):
+        self.http_client = HTTPClient('http://10.10.10.10:8080',
+                                      username='admin',
+                                      password='admin')
 
-        http = odlclient.HTTPClient('http://10.10.10.10:8080',
-                                    username='admin',
-                                    password='admin')
+    def test_client(self):
 
-        self.client = odlclient.Client(http)
-        response_ok = requests.Response()
-        response_ok.status_code = 200
-        self.response_ok = response_ok
+        http_client = self.http_client
 
-    def test_post(self):
-        self.client.post = MagicMock(name="post",
-                                     return_value=self.response_ok)
+        api = Client(http_client)
+        self.assertTrue(isinstance(api.bridge_domain, BridgeDomainManager))
+        self.assertTrue(isinstance(api.connection_manager, ConnectionManager))
+        self.assertTrue(isinstance(api.nodes, NodeManager))
+        self.assertTrue(isinstance(api.ovsdb, OvsdbManager))
+        self.assertTrue(isinstance(api.subnets, SubnetManager))
+        self.assertTrue(isinstance(api.staticroutes, StaticRouteManager))
+        self.assertTrue(isinstance(api.neutron, NeutronManagers))
 
-        r = self.client.post('http://foo.bar', json.dumps({"some": "data"}))
+    def test_httpclient(self):
+        http_client = self.http_client
 
-        self.client.post.assert_called_with('http://foo.bar',
-                                            json.dumps({"some": "data"}))
-
-        self.assertTrue(isinstance(r, requests.Response))
-
-    def test_put(self):
-        self.client.put = MagicMock(name="put",
-                                    return_value=self.response_ok)
-
-        r = self.client.put('http://foo.bar', json.dumps({"some": "data"}))
-
-        self.client.put.assert_called_with('http://foo.bar',
-                                           json.dumps({"some": "data"}))
-
-        self.assertTrue(isinstance(r, requests.Response))
-
-    def test_delete_data(self):
-        self.client.delete = MagicMock(name="delete",
-                                       return_value=self.response_ok)
-
-        r = self.client.delete('http://foo.bar', json.dumps({"some": "data"}))
-
-        self.client.delete.assert_called_with('http://foo.bar',
-                                              json.dumps({"some": "data"}))
-
-        self.assertTrue(isinstance(r, requests.Response))
-
-    def test_delete_no_data(self):
-        self.client.delete = MagicMock(name="delete",
-                                       return_value=self.response_ok)
-
-        r = self.client.delete('http://foo.bar')
-
-        self.client.delete.assert_called_with('http://foo.bar')
-
-        self.assertTrue(isinstance(r, requests.Response))
-
-    def test_head(self):
-        self.client.head = MagicMock(name="head",
-                                     return_value=self.response_ok)
-        #self.raise_errors = MagicMock(name="raise_errors")
-
-        r = self.client.head('http://foo.bar')
-
-        self.client.head.assert_called_with('http://foo.bar')
-
-        self.assertTrue(isinstance(r, requests.Response))
+        self.assertEqual(http_client.endpoint, 'http://10.10.10.10:8080')
+        self.assertEqual(http_client.username, 'admin')
+        self.assertEqual(http_client.password, 'admin')
---
+++ odlclient/tests/unit/test_client_rest.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+#
+#   Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import json
+import unittest
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import MagicMock
+
+import requests
+
+
+from odlclient.v2 import client as odlclient
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+
+        http = odlclient.HTTPClient('http://10.10.10.10:8080',
+                                    username='admin',
+                                    password='admin')
+
+        self.client = odlclient.Client(http)
+        response_ok = requests.Response()
+        response_ok.status_code = 200
+        self.response_ok = response_ok
+
+    def test_post(self):
+        self.client.post = MagicMock(name="post",
+                                     return_value=self.response_ok)
+
+        r = self.client.post('http://foo.bar', json.dumps({"some": "data"}))
+
+        self.client.post.assert_called_with('http://foo.bar',
+                                            json.dumps({"some": "data"}))
+
+        self.assertTrue(isinstance(r, requests.Response))
+
+    def test_put(self):
+        self.client.put = MagicMock(name="put",
+                                    return_value=self.response_ok)
+
+        r = self.client.put('http://foo.bar', json.dumps({"some": "data"}))
+
+        self.client.put.assert_called_with('http://foo.bar',
+                                           json.dumps({"some": "data"}))
+
+        self.assertTrue(isinstance(r, requests.Response))
+
+    def test_delete_data(self):
+        self.client.delete = MagicMock(name="delete",
+                                       return_value=self.response_ok)
+
+        r = self.client.delete('http://foo.bar', json.dumps({"some": "data"}))
+
+        self.client.delete.assert_called_with('http://foo.bar',
+                                              json.dumps({"some": "data"}))
+
+        self.assertTrue(isinstance(r, requests.Response))
+
+    def test_delete_no_data(self):
+        self.client.delete = MagicMock(name="delete",
+                                       return_value=self.response_ok)
+
+        r = self.client.delete('http://foo.bar')
+
+        self.client.delete.assert_called_with('http://foo.bar')
+
+        self.assertTrue(isinstance(r, requests.Response))
+
+    def test_head(self):
+        self.client.head = MagicMock(name="head",
+                                     return_value=self.response_ok)
+        #self.raise_errors = MagicMock(name="raise_errors")
+
+        r = self.client.head('http://foo.bar')
+
+        self.client.head.assert_called_with('http://foo.bar')
+
+        self.assertTrue(isinstance(r, requests.Response))
